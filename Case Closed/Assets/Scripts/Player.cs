d218f138405e6075bd9bf676a5333ae8a4da917d
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-06-20 23:15:31 +0530

    First commit

---
+++ Case Closed/Assets/Scripts/Player.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float moveSpeed;
+
+
+    Vector2 playerInput;
+    Rigidbody2D playerRb;
+
+    BoxCollider2D playerFeetCollider;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerFeetCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        Run();
+        Flip();
+    }
+
+    void Run()
+    {
+
+        //if (playerInput.x != 0)
+        //{
+        //    playerAnim.SetBool("isRunning", true);
+        //}
+        //else
+        //{
+        //    playerAnim.SetBool("isRunning", false);
+        //}
+        playerRb.linearVelocity = new Vector2(playerInput.x * moveSpeed, playerRb.linearVelocity.y);
+    }
+
+
+    void OnMove(InputValue input)
+    {
+        playerInput = input.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!playerFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (value.isPressed)
+        {
+            playerRb.linearVelocity = new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void Flip()
+    {
+
+        if (playerRb.linearVelocity.x < 0 && isFacingRight == true)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            isFacingRight = false;
+        }
+        else if (playerRb.linearVelocity.x > 0 && isFacingRight == false)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            isFacingRight = true;
+        }
+
+    }
+}
